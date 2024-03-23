ac6b0b50c9de94fdcce5a602784c73bc5e293c6b
Vinicius Louzada <vinelouzada@gmail.com>
2024-03-23 12:53:40 -0300

    build: Spring Boot basic configuration - project structure and dependencies

---
+++ src/main/java/br/com/alura/alurex/api/AlurexApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurex.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlurexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlurexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/alurex/api/AlurexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlurexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
